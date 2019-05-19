80cebe1202823ae1747b6a5dce1c1d7f3f3484c5
Sindhu <sindhugudivada67@gmail.com>
2019-05-18 20:34:09 -0700

    Solved Eulers problem Collatz Sequence.
    525 is the maximum number of jumps to converge to 1

--- CollatzSequence.swift
+++ CollatzSequence.swift
@@ -1,6 +1,4 @@
-import UIKit
-
-func getNthNumber(_ n: Int) -> Int {
+func getJumpsCountToConverge(_ n: Int) -> Int {
     var count = 0
     var value = n
 
@@ -13,4 +11,34 @@ func getNthNumber(_ n: Int) -> Int {
         count = count + 1
     }
 return count
+}
+
+func getJumpsCountToConvergeRecursive(_ n: Int) -> Int {
+
+  if n == 1 { return 0 }
+  if n%2 == 0 {
+    return 1 + getJumpsCountToConvergeRecursive(Int(n/2))
+   } else {
+     return (1 + getJumpsCountToConvergeRecursive((3*n)+1))
+   }
+}
+
+func maxNoOfJumps(_ n: Int) -> Int {
+  var result = 0
+  for i in 1..<n {
+      let value = getJumpsCountToConverge(i)
+      result = max(value,result)
+  }
+return result  
+}
+
+func getNumWithMaxJumps(_ n: Int) -> Int {
+    var jumpsCache: [Int:Int] = [:]
+    var maxJumps = 1
+    for i in 1..<n {
+      let value = getJumpsCountToConvergeRecursive(i)
+      jumpsCache[value] = i
+      maxJumps = max(value,maxJumps)
+    }
+  return jumpsCache[maxJumps]!  
 }
